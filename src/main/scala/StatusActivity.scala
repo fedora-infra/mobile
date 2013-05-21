d3fa11935383abfd3178507d4aa388a1caeb9ab0
Ricky Elrod <ricky@elrod.me>
2013-05-21 01:08:30 -0400

    Make Status render reasonably well.

--- src/main/scala/StatusActivity.scala
+++ src/main/scala/StatusActivity.scala
@@ -3,9 +3,12 @@ package org.fedoraproject.mobile
 import Implicits._
 
 import android.app.Activity
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.view.{ LayoutInflater, View, ViewGroup }
 import android.widget.AdapterView.OnItemClickListener
-import android.widget.{ AdapterView, ArrayAdapter }
+import android.widget.{ AdapterView, ArrayAdapter, LinearLayout, TextView }
 
 import spray.json._
 
@@ -34,14 +37,53 @@ class StatusActivity extends NavDrawerActivity {
         case Success(e) => {
           val parsed = JsonParser(e).convertTo[StatusesResponse]
 
-          val entries = parsed.services.map { case (service, info) =>
-            service + " - " + info("status")
+          class StatusAdapter(
+            context: Context,
+            resource: Int,
+            items: Array[String])
+          extends ArrayAdapter[String](context, resource, items) {
+            override def getView(position: Int, convertView: View, parent: ViewGroup): View = {
+              val shortname = getItem(position)
+              val service = parsed.services(shortname)
+
+              val layout = LayoutInflater.from(context)
+                .inflate(R.layout.status_list_item, parent, false)
+                .asInstanceOf[LinearLayout]
+
+              layout
+                .findViewById(R.id.servicename)
+                .asInstanceOf[TextView]
+                .setText(service("name"))
+
+              layout
+                .findViewById(R.id.serviceshortname)
+                .asInstanceOf[TextView]
+                .setText(shortname)
+
+              layout
+                .findViewById(R.id.servicestatus)
+                .asInstanceOf[TextView]
+                .tap { obj =>
+                  obj.setText(service("status") match {
+                    case "good" => R.string.status_good
+                    case "minor" => R.string.status_minor
+                    case "major" => R.string.status_major
+                  })
+                  obj.setTextColor(service("status") match {
+                    case "good" => Color.parseColor("#009900")
+                    case "minor" => Color.parseColor("#ffcc00")
+                    case "major" => Color.parseColor("#990000")
+                  })
+                }
+
+              layout
+            }
           }
 
-          val adapter = new ArrayAdapter[String](
+          val adapter = new StatusAdapter(
             this,
             android.R.layout.simple_list_item_1,
-            entries.toArray)
+            parsed.services.toArray.sortBy(_._2("name")).map(_._1))
 
           runOnUiThread {
             findView(TR.statuses).setAdapter(adapter)
