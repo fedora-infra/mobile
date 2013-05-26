d6978a4b302266150fa307117665f9b4fb5e86a1
Ricky Elrod <ricky@elrod.me>
2013-05-26 19:18:25 -0400

    Nav drawer UI redesign. Dark theme, icons.

--- src/main/scala/NavDrawerActivity.scala
+++ src/main/scala/NavDrawerActivity.scala
@@ -1,18 +1,18 @@
 package org.fedoraproject.mobile
 
 import android.app.Activity
-import android.content.Intent
+import android.content.{ Context, Intent }
 import android.content.res.Configuration
 import android.os.Bundle
 import android.support.v4.app.{ ActionBarDrawerToggle, FragmentActivity }
-import android.view.View
-import android.widget.{ AdapterView, ArrayAdapter }
+import android.view.{ LayoutInflater, View, ViewGroup }
+import android.widget.{ AdapterView, ArrayAdapter, ImageView, LinearLayout, TextView }
 
 trait NavDrawerActivity extends FragmentActivity with TypedActivity {
 
   val navMap = Map(
-    "Infrastructure Status" -> classOf[StatusActivity],
-    "Package Search" -> classOf[PackageSearchActivity]
+    "Infrastructure Status" -> (classOf[StatusActivity], R.drawable.ic_menu_star),
+    "Package Search" -> (classOf[PackageSearchActivity], R.drawable.ic_menu_search)
   )
 
   override def onCreate(bundle: Bundle) {
@@ -45,16 +45,43 @@ trait NavDrawerActivity extends FragmentActivity with TypedActivity {
     getActionBar.setDisplayHomeAsUpEnabled(true);
     getActionBar.setHomeButtonEnabled(true);
 
+    class NavAdapter(
+      context: Context,
+      resource: Int,
+      items: Array[String])
+      extends ArrayAdapter[String](context, resource, items) {
+        override def getView(position: Int, convertView: View, parent: ViewGroup): View = {
+          val key = getItem(position)
+          val value = navMap(key)
+
+          val layout = LayoutInflater.from(context)
+            .inflate(R.layout.drawer_list_item, parent, false)
+            .asInstanceOf[LinearLayout]
+
+          layout
+            .findViewById(R.id.icon)
+            .asInstanceOf[ImageView]
+            .setImageResource(value._2)
+
+          layout
+            .findViewById(R.id.title)
+            .asInstanceOf[TextView]
+            .setText(key)
+
+          layout
+        }
+    }
+
     drawerList.setAdapter(
-      new ArrayAdapter[String](
+      new NavAdapter(
         this,
-        R.layout.drawer_list_item,
+        android.R.layout.simple_list_item_1,
         navMap.keys.toArray))
 
     drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener {
       def onItemClick(parent: AdapterView[_], view: View, position: Int, id: Long) {
         val keys = navMap.keys.toArray
-        val intent = new Intent(NavDrawerActivity.this, navMap(keys(position)))
+        val intent = new Intent(NavDrawerActivity.this, navMap(keys(position))._1)
         startActivity(intent)
       }
     })
