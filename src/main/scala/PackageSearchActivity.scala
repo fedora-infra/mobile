7e60747b763f5fcf90ba1c13e91c4fa68d726af6
Ricky Elrod <ricky@elrod.me>
2013-05-30 12:51:11 -0400

    Attempt package search without pkgwat-scala.
    Right now, Dispatch won't work on Android
    ( see https://groups.google.com/forum/#!topic/dispatch-scala/pJC5fwec-pA ).
    
    With that in mind, I re-implemented the search functionality of
    pkgwat-scala without Dispatch for now, just so that I can start
    working on the UI for some of this.
    
    If/when we get Dispatch working, it'll be easy to switch back to it
    and a lot of this will become less hacky.

--- src/main/scala/PackageSearchActivity.scala
+++ src/main/scala/PackageSearchActivity.scala
@@ -3,19 +3,152 @@ package org.fedoraproject.mobile
 import Implicits._
 
 import android.app.{ Activity, SearchManager }
-import android.content.Context
+import android.content.{ Context, Intent }
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.{ LayoutInflater, Menu, View, ViewGroup }
 import android.widget.AdapterView.OnItemClickListener
-import android.widget.{ AdapterView, ArrayAdapter, LinearLayout, TextView, Toast, SearchView }
+import android.widget.{ AdapterView, ArrayAdapter, ImageView, LinearLayout, ListView, TextView, Toast, SearchView }
 
-import scala.concurrent._
-import scala.concurrent.duration._
+import spray.json._
+import java.net.{ URL, URLEncoder }
+
+import scala.concurrent.future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.io.Source
+import scala.util.{ Failure, Try, Success }
+
+import java.io.InputStream
+
+case class APIResults[T](
+  visibleRows: Int,
+  totalRows: Int,
+  rowsPerPage: Int,
+  startRow: Int,
+  rows: List[T])
+
+case class Package(
+  icon: String,
+  description: String,
+  link: String,
+  subPackages: Option[List[Package]],
+  summary: String,
+  name: String,
+  upstreamURL: Option[String] = None,
+  develOwner: Option[String] = None)
+
+case class FilteredQuery(rowsPerPage: Int, startRow: Int, filters: Map[String, String])
+
+object PackagesJsonProtocol extends DefaultJsonProtocol {
+  implicit val filteredQueryFormat = jsonFormat(FilteredQuery, "rows_per_page", "start_row", "filters")
+  implicit val packageFormat: JsonFormat[Package] = lazyFormat(jsonFormat(Package, "icon", "description", "link", "sub_pkgs", "summary", "name", "upstream_url", "devel_owner"))
+  implicit val packageResultFormat = jsonFormat(APIResults[Package], "visible_rows", "total_rows", "rows_per_page", "start_row", "rows")
+}
+
+import PackagesJsonProtocol._
 
 class PackageSearchActivity extends NavDrawerActivity {
   override def onCreate(bundle: Bundle) {
     super.onCreate(bundle)
-    setUpNav(R.layout.main)
+    setUpNav(R.layout.package_search)
+    handleIntent(getIntent())
+  }
+
+  override def onNewIntent(intent: Intent) {
+    setIntent(intent)
+    handleIntent(intent)
+  }
+
+  private def constructURL(path: String, query: FilteredQuery): String = {
+    val json = query.toJson.compactPrint
+    Seq(
+      "https://apps.fedoraproject.org/packages/",
+      "fcomm_connector",
+      path,
+      URLEncoder.encode(json, "utf8")).mkString("/")
+  }
+
+  def handleIntent(intent: Intent) {
+    if (intent.getAction == Intent.ACTION_SEARCH) {
+      val query = intent.getStringExtra(SearchManager.QUERY)
+      val jsonURL = constructURL(
+        "xapian/query/search_packages",
+        FilteredQuery(
+          500, // TODO: Pagination...and, well, unhacking this.
+          0,
+          Map("search" -> query)))
+
+        future {
+          Source.fromURL(jsonURL).mkString
+        } onComplete { result =>
+          result match {
+            case Success(content) => {
+              val result = JsonParser(content.replaceAll("""<\/?.*?>""", "")).convertTo[APIResults[Package]]
+              val packages = result.rows.toArray
+
+              class PackageAdapter(
+                context: Context,
+                resource: Int,
+                items: Array[Package])
+                extends ArrayAdapter[Package](context, resource, items) {
+                  override def getView(position: Int, convertView: View, parent: ViewGroup): View = {
+                    val pkg = getItem(position)
+
+                    val layout = LayoutInflater.from(context)
+                      .inflate(R.layout.package_list_item, parent, false)
+                      .asInstanceOf[LinearLayout]
+
+                    future {
+                      val url = new URL(s"https://apps.fedoraproject.org/packages/images/icons/${pkg.icon}.png")
+                      BitmapFactory.decodeStream(url.getContent.asInstanceOf[InputStream])
+                    } onComplete { result =>
+                      result match {
+                        case Success(icon) => {
+                          runOnUiThread {
+                            layout
+                              .findViewById(R.id.icon)
+                              .asInstanceOf[ImageView]
+                              .setImageBitmap(icon)
+                          }
+                        }
+                        case Failure(error) => {
+                          runOnUiThread {
+                            layout
+                              .findViewById(R.id.icon)
+                              .asInstanceOf[ImageView]
+                              .setImageResource(R.drawable.ic_search)
+                          }
+                        }
+                      }
+                    }
+
+                    layout
+                      .findViewById(R.id.title)
+                      .asInstanceOf[TextView]
+                      .setText(pkg.name)
+
+                    layout
+                  }
+              }
+
+              val adapter = new PackageAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                packages)
+
+              runOnUiThread {
+                val packagesView = Option(findView(TR.packages))
+                packagesView match {
+                  case Some(packagesView) => packagesView.asInstanceOf[ListView].setAdapter(adapter)
+                  case None => // ...
+                }
+              }
+            }
+            case Failure(e) => Toast.makeText(this, R.string.packages_search_failure, Toast.LENGTH_LONG).show
+          }
+      }
+    }
   }
 
   override def onCreateOptionsMenu(menu: Menu) = {
