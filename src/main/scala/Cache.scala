3ec10c48e9f627b8d15d8126dcc684f3d4315d26
Ricky Elrod <ricky@elrod.me>
2013-06-12 14:12:32 -0400

    [Cache] Make a this a lot more modular/reusable.

--- src/main/scala/Cache.scala
+++ src/main/scala/Cache.scala
@@ -11,45 +11,59 @@ import java.io.{ File, BufferedInputStream, BufferedOutputStream, FileInputStrea
 import java.net.{ URL, URLEncoder }
 
 object Cache {
-  /** Returns a [[Future[Bitmap]]] containing the icon for a package.
+  /** Returns a [[Future[Bitmap]]] containing an image, given parameters.
     *
-    * This method will check to see if the image is cached. If it is, it will
-    * not bother making an HTTP request for the image. If it is not, it will
-    * get the image, save it to cache, then load it from cache.
+    * This method provides general purpose access to the cache, given a set of
+    * parameters that remain consistent between accesses.
     *
-    * @param context The context on which to act.
-    * @param path The name of the image on the server (and in cache).
+    * The cache is "general purpose" in that you give it a URL and some other
+    * parameters, and it will cache the image how you tell it to, rather than
+    * needing to duplicate logic everywhere.
+    *
+    * This will download the image from the url given, if a path in the cache
+    * directory with the same name doesn't exist already. Then it will return
+    * the (now-cached, if necessary) cached image from the cache. Yo dawg.
+    *
+    * @param context The Android context on which to act.
+    * @param url A [[java.net.URL]] that is the image's URL.
+    * @param directoryName The directory in cache to store the image in.
+    * @param fileName The name of the file to store in the cache.
     */
-  def getPackageIcon(context: Context, path: String): Future[Bitmap] = {
+  private def cacheImage(context: Context, url: URL, directoryName: String, fileName: String) =
     future {
       val cache = context.getCacheDir
-      val iconsDir = new File(cache.toString + "/package_icons")
-      iconsDir.mkdir
-      val iconFile = new File(iconsDir.toString + "/" + path)
-      if (iconFile.exists) {
-        Log.v("PackageSearchActivity", "Icon already cached.")
+      val directory = new File(cache.toString + "/" + directoryName)
+      directory.mkdir
+      val file = new File(directory.toString + "/" + fileName)
+      if (file.exists) {
+        Log.v("Cache.cacheImage", s"$directoryName/$fileName already cached.")
       } else {
-        Log.v("PackageSearchActivity", "Icon pulled from HTTP.")
-        val url = new URL(s"https://apps.fedoraproject.org/packages/images/icons/${path}.png")
-          .getContent
-          .asInstanceOf[InputStream]
-        val outputStream = new BufferedOutputStream(new FileOutputStream(iconFile))
+        Log.v("Cache.cacheImage", s"$url pulled from HTTP.")
+        val urlStream = url.getContent.asInstanceOf[InputStream]
+        val outputStream = new BufferedOutputStream(new FileOutputStream(file))
         Iterator
-          .continually(url.read)
+          .continually(urlStream.read)
           .takeWhile(-1 !=)
           .foreach(outputStream.write)
         outputStream.flush
       }
-      val inputStream = new BufferedInputStream(new FileInputStream(iconFile))
+      val inputStream = new BufferedInputStream(new FileInputStream(file))
       BitmapFactory.decodeStream(inputStream)
     }
-  }
+
+  /** Returns a [[Future[Bitmap]]] containing the icon for a package.
+    *
+    * @param context The context on which to act.
+    * @param path The name of the image on the server (and in cache).
+    */
+  def getPackageIcon(context: Context, path: String): Future[Bitmap] =
+    cacheImage(
+      context,
+      new URL(s"https://apps.fedoraproject.org/packages/images/icons/${path}.png"),
+      "package_icons",
+      path)
 
   /** Returns a [[Future[Bitmap]]] containing a Gravatar.
-    *
-    * Like getPackageIcon, this method will check to see if the image is cached.
-    * If it is, it will not bother making an HTTP request for the image. If it
-    * is not, it will get the image, save it to cache, then load it from cache.
     *
     * @param context The context on which to act.
     * @param md5sum A md5sum of the email address to look up.
@@ -61,29 +75,11 @@ object Cache {
     md5sum: String,
     size: Int = 64,
     default: String = "https://fedoraproject.org/static/images/fedora_infinity_64x64.png"): Future[Bitmap] = {
-    future {
-      val cache = context.getCacheDir
-      val dir = new File(cache.toString + "/gravatar")
-      dir.mkdir
-      val file = new File(dir.toString + "/" + md5sum + ".jpg")
-      if (file.exists) {
-        Log.v(context.getClass.getSimpleName, "Gravatar already cached.")
-      } else {
-        Log.v(context.getClass.getSimpleName, "Gravatar pulled from HTTP.")
-        val defaultEncoded = URLEncoder.encode(default, "utf8")
-        val url = new URL(
-          s"https://secure.gravatar.com/avatar/$md5sum?s=$size&d=$defaultEncoded")
-          .getContent
-          .asInstanceOf[InputStream]
-        val outputStream = new BufferedOutputStream(new FileOutputStream(file))
-        Iterator
-          .continually(url.read)
-          .takeWhile(-1 !=)
-          .foreach(outputStream.write)
-        outputStream.flush
-      }
-      val inputStream = new BufferedInputStream(new FileInputStream(file))
-      BitmapFactory.decodeStream(inputStream)
-    }
+      val defaultEncoded = URLEncoder.encode(default, "utf8")
+      cacheImage(
+        context,
+        new URL(s"https://secure.gravatar.com/avatar/$md5sum?s=$size&d=$defaultEncoded"),
+        "gravatar",
+        md5sum)
   }
 }
