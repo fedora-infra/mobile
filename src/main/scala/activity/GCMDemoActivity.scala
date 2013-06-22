58a8849e15458b806fedd92765953011478fb8b3
Ricky Elrod <ricky@elrod.me>
2013-06-22 01:11:19 -0400

    Send the registration id to Sprunge for now.
    When we switch to fedmsg-notifications and move everything to production
    this will go away. Right now it's pretty hacky. You end up having to
    set up the fedmsg-gcm-demo daemon locally, getting an "Sender ID" from the
    Google API console, typing that into Fedora Mobile, and then getting a
    "Registration ID" back in the form of a sprunge.us link. Once you have that,
    you have to copy it into a YAML file.
    
    This is hugely a proof of concept, just because I want to be able to demo this
    stuff at Flock in August.

--- src/main/scala/activity/GCMDemoActivity.scala
+++ src/main/scala/activity/GCMDemoActivity.scala
@@ -1,13 +1,19 @@
 package org.fedoraproject.mobile
 
+import Implicits._
+
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 
 import com.google.android.gms.gcm.GoogleCloudMessaging
+import com.google.common.io.CharStreams
 
-import scala.concurrent.future
+import scala.concurrent.{Future, future}
 import scala.concurrent.ExecutionContext.Implicits.global
+
+import java.io.{DataOutputStream, InputStreamReader}
+import java.net.{HttpURLConnection, URL, URLEncoder}
 
 class GCMDemoActivity extends NavDrawerActivity {
   override def onPostCreate(bundle: Bundle) {
@@ -17,8 +23,26 @@ class GCMDemoActivity extends NavDrawerActivity {
     future {
       val gcm = GoogleCloudMessaging.getInstance(this)
       gcm.register("677116861562")
-    }.onSuccess { case result =>
-      Toast.makeText(this, result + " FOO", Toast.LENGTH_LONG).show
+    } onSuccess { case result =>
+      sprungeRegistrationId(result) onSuccess { case sprungeURL =>
+        runOnUiThread {
+          Toast.makeText(this, sprungeURL, Toast.LENGTH_LONG).show
+        }
+      }
+    }
+  }
+
+  // POSTing is so much fun with Java's stdlib.
+  private def sprungeRegistrationId(id: String): Future[String] = {
+    future {
+      val connection = new URL("http://sprunge.us/").openConnection.asInstanceOf[HttpURLConnection]
+      connection setDoOutput true
+      connection setRequestMethod "POST"
+      val os = new DataOutputStream(connection.getOutputStream)
+      val encodedID = URLEncoder.encode(id, "utf8")
+      os.writeBytes(s"sprunge=${encodedID}")
+      os.close
+      CharStreams.toString(new InputStreamReader(connection.getInputStream, "utf8"))
     }
   }
 
