a33384e8bb818bac16f2baa04ad9c4b72cfe1586
Ricky Elrod <ricky@elrod.me>
2013-06-25 19:20:07 -0400

    [GDA] Unhardcode the sender ID

--- src/main/scala/activity/GCMDemoActivity.scala
+++ src/main/scala/activity/GCMDemoActivity.scala
@@ -19,19 +19,6 @@ class GCMDemoActivity extends NavDrawerActivity {
   override def onPostCreate(bundle: Bundle) {
     super.onPostCreate(bundle)
     setUpNav(R.layout.gcm_demo_activity)
-
-    future {
-      val gcm = GoogleCloudMessaging.getInstance(this)
-      gcm.register("677116861562")
-    } onSuccess {
-      case result =>
-        sprungeRegistrationId(result) onSuccess {
-          case sprungeURL =>
-            runOnUiThread {
-              Toast.makeText(this, sprungeURL, Toast.LENGTH_LONG).show
-            }
-        }
-    }
   }
 
   // POSTing is so much fun with Java's stdlib.
@@ -47,6 +34,17 @@ class GCMDemoActivity extends NavDrawerActivity {
   }
 
   def register(view: View): Unit = {
-    //Toast.makeText(this, registrationID, Toast.LENGTH_LONG).show
+    future {
+      val gcm = GoogleCloudMessaging.getInstance(this)
+      gcm.register(findView(TR.sender_id).getText.toString)
+    } onSuccess {
+      case result =>
+        sprungeRegistrationId(result) onSuccess {
+          case sprungeURL =>
+            runOnUiThread {
+              Toast.makeText(this, sprungeURL, Toast.LENGTH_LONG).show
+            }
+        }
+    }
   }
 }
