c4ef2f07cb89eec7c6c79959780e6f86df5e1fe4
Ricky Elrod <ricky@elrod.me>
2013-07-04 17:43:09 -0400

    NewsfeedAdapter -> FedmsgAdapter, and move it.
    In an attempt to clean up our activities and have reusable pieces where we
    can, I'm going to move our list adapters out to an adapter directory.
    
    This does that with the NewsfeedAdapter which has been renamed to
    FedmsgAdapter, because it's less general than every possible newsfeed
    we might one day have.

--- src/main/scala/activity/MainActivity.scala
+++ src/main/scala/activity/MainActivity.scala
@@ -2,20 +2,16 @@ package org.fedoraproject.mobile
 
 import Implicits._
 
-import android.content.Context
 import android.os.Bundle
-import android.view.{ LayoutInflater, Menu, MenuItem, View, ViewGroup }
-import android.widget.{ ArrayAdapter, ImageView, LinearLayout, ListView, TextView, Toast }
+import android.view.{ Menu, MenuItem, View }
 
 import spray.json._
 import scala.concurrent.{ future, Future }
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.io.Source
-import scala.util.{ Failure, Try, Success }
+
 
 import Datagrepper.JSONParsing._
-
-import com.google.common.hash.Hashing
 
 import java.text.SimpleDateFormat
 import java.util.TimeZone
@@ -47,67 +43,6 @@ class MainActivity extends NavDrawerActivity {
       HRF(res.messages.toString, TimeZone.getDefault)
     }
 
-    class NewsfeedAdapter(
-      context: Context,
-      resource: Int,
-      items: Array[HRF.Result])
-      extends ArrayAdapter[HRF.Result](context, resource, items) {
-
-      override def getView(position: Int, convertView: View, parent: ViewGroup): View = {
-        val item = getItem(position)
-
-        val layout = LayoutInflater.from(context)
-          .inflate(R.layout.fedmsg_list_item, parent, false)
-          .asInstanceOf[LinearLayout]
-
-        val iconView = layout
-          .findViewById(R.id.icon)
-          .asInstanceOf[ImageView]
-
-        // If there's a user associated with it, pull from gravatar.
-        // Otherwise, just use the icon if it exists.
-        if (item.usernames.length > 0) {
-          val bytes = s"${item.usernames.head}@fedoraproject.org".getBytes("utf8")
-          Cache.getGravatar(
-            MainActivity.this,
-            Hashing.md5.hashBytes(bytes).toString).onComplete { result =>
-              result match {
-                case Success(gravatar) => {
-                  runOnUiThread {
-                    iconView.setImageBitmap(gravatar)
-                  }
-                }
-                case _ =>
-              }
-            }
-        } else {
-          item.icon match {
-            case Some(icon) => {
-              Cache.getServiceIcon(MainActivity.this, icon, item.title) onSuccess {
-                case icon =>
-                  runOnUiThread {
-                    iconView.setImageBitmap(icon)
-                  }
-              }
-            }
-            case None =>
-          }
-        }
-
-        layout
-          .findViewById(R.id.subtitle)
-          .asInstanceOf[TextView]
-          .setText(item.subtitle)
-
-        layout
-          .findViewById(R.id.timestamp)
-          .asInstanceOf[TextView]
-          .setText(item.timestamp("usadate") + " at " + item.timestamp("time"))
-
-        layout
-      }
-    }
-
     messages onSuccess {
       case res =>
         // Yo dawg. I heard you like futures. So I put futures in your future.
@@ -117,7 +52,7 @@ class MainActivity extends NavDrawerActivity {
             runOnUiThread {
               findView(TR.progress).setVisibility(View.GONE)
             }
-            val adapter = new NewsfeedAdapter(
+            val adapter = new FedmsgAdapter(
               this,
               android.R.layout.simple_list_item_1,
               hrfResult.toArray)
---
+++ src/main/scala/adapter/FedmsgAdapter.scala
@@ -0,0 +1,77 @@
+package org.fedoraproject.mobile
+
+import Implicits._
+
+import android.app.Activity
+import android.content.Context
+import android.view.{ LayoutInflater, View, ViewGroup }
+import android.widget.{ ArrayAdapter, ImageView, LinearLayout, ListView, TextView, Toast }
+
+import scala.concurrent.{ future, Future }
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{ Failure, Try, Success }
+
+import com.google.common.hash.Hashing
+
+class FedmsgAdapter(
+  context: Context,
+  resource: Int,
+  items: Array[HRF.Result])
+  extends ArrayAdapter[HRF.Result](context, resource, items) {
+
+  val activity = context.asInstanceOf[Activity]
+
+  override def getView(position: Int, convertView: View, parent: ViewGroup): View = {
+    val item = getItem(position)
+
+    val layout = LayoutInflater.from(context)
+      .inflate(R.layout.fedmsg_list_item, parent, false)
+      .asInstanceOf[LinearLayout]
+
+    val iconView = layout
+      .findViewById(R.id.icon)
+      .asInstanceOf[ImageView]
+
+    // If there's a user associated with it, pull from gravatar.
+    // Otherwise, just use the icon if it exists.
+    if (item.usernames.length > 0) {
+      val bytes = s"${item.usernames.head}@fedoraproject.org".getBytes("utf8")
+      Cache.getGravatar(
+        context,
+        Hashing.md5.hashBytes(bytes).toString).onComplete { result =>
+          result match {
+            case Success(gravatar) => {
+              activity.runOnUiThread {
+                iconView.setImageBitmap(gravatar)
+              }
+            }
+            case _ =>
+          }
+        }
+    } else {
+      item.icon match {
+        case Some(icon) => {
+          Cache.getServiceIcon(context, icon, item.title) onSuccess {
+            case icon =>
+              activity.runOnUiThread {
+                iconView.setImageBitmap(icon)
+              }
+          }
+        }
+        case None =>
+      }
+    }
+
+    layout
+      .findViewById(R.id.subtitle)
+      .asInstanceOf[TextView]
+      .setText(item.subtitle)
+
+    layout
+      .findViewById(R.id.timestamp)
+      .asInstanceOf[TextView]
+      .setText(item.timestamp("usadate") + " at " + item.timestamp("time"))
+
+    layout
+  }
+}
