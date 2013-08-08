3cc8324988c2858401294b2fc2f4990c00e5d928
Ricky Elrod <ricky@elrod.me>
2013-08-08 04:37:58 -0400

    Lay groundwork for Badges UI

--- src/main/scala/activity/BadgesActivity.scala
+++ src/main/scala/activity/BadgesActivity.scala
@@ -1,10 +1,23 @@
 package org.fedoraproject.mobile
 
+import Badges.JSONParsing._
+
+import Implicits._
+
 import android.os.Bundle
+
+import spray.json._
+
+// import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshAttacher
+
+import scala.concurrent.{ future, Future }
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.io.Source
+import scala.util.{ Failure, Success }
 
 class BadgesActivity extends NavDrawerActivity {
   override def onPostCreate(bundle: Bundle) {
     super.onPostCreate(bundle)
-    setUpNav(R.layout.main_activity)
+    setUpNav(R.layout.badges_activity)
   }
 }
---
+++ src/main/scala/webapi/Badges.scala
@@ -0,0 +1,61 @@
+package org.fedoraproject.mobile
+
+import android.net.Uri
+import android.util.Log
+
+import com.google.common.io.CharStreams
+
+import spray.json._
+
+import scala.concurrent.{ future, Future }
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.io.Source
+import scala.util.{ Failure, Try, Success }
+
+import java.io.{ DataOutputStream, InputStreamReader }
+import java.net.{ HttpURLConnection, URL, URLEncoder }
+
+object Badges {
+  val url = "http://10.10.10.125:8000/"
+
+  case class Badge(
+    id: String,
+    name: String,
+    image: String,
+    firstAwarded: Option[Float],
+    firstAwardedPerson: Option[String],
+    lastAwarded: Option[Float],
+    lastAwardedPerson: Option[String],
+    percentEarned: Float,
+    timesAwarded: Int,
+    description: String,
+    issued: Option[String] = None // Only in the 'user' JSON.
+  )
+
+  case class User(
+    percentEarned: Float,
+    user: String,
+    assertions: List[Badge])
+
+
+  object JSONParsing extends DefaultJsonProtocol {
+    implicit val badgeResponse = jsonFormat(Badge, "id", "name", "image", "first_awarded", "first_awarded_person", "last_awarded", "last_awarded_person", "percent_earned", "times_awarded", "description", "issued")
+    implicit val userResponse = jsonFormat(User, "percent_earned", "user", "assertions")
+  }
+
+   /** Returns a [[Future[String]]] of JSON after completing the query. */
+  def query(path: String) = {
+    Log.v("Badges", "Beginning query")
+    val uri = Uri.parse(url + path.dropWhile(_ == '/'))
+    future {
+      val connection = new URL(uri.toString)
+        .openConnection
+        .asInstanceOf[HttpURLConnection]
+      connection setRequestMethod "GET"
+      CharStreams.toString(new InputStreamReader(connection.getInputStream, "utf8"))
+    }
+  }
+
+  def forUser(user: String): Future[String] = query(s"/user/${user}/json")
+  def info(id: String): Future[String] = query(s"/badge/${id}/json")
+}
