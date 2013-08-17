dd36c136f38f8ee2b91e241834a0a5bc0cc11cac
Ricky Elrod <ricky@elrod.me>
2013-08-17 01:20:49 -0400

    [BIA] Show user's nickname/gravatar in actionbar

--- src/main/scala/activity/BadgesUserActivity.scala
+++ src/main/scala/activity/BadgesUserActivity.scala
@@ -4,11 +4,14 @@ import Badges.JSONParsing._
 
 import Implicits._
 
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 
 import spray.json._
+
+import com.google.common.hash.Hashing
 
 // import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshAttacher
 
@@ -25,6 +28,23 @@ class BadgesUserActivity extends NavDrawerActivity {
     // The nickname is passed via the intent. If it's not there, something went
     // horribly wrong and fataling is probably the correct thing to do.
     val nickname = getIntent.getExtras.getString("nickname")
+
+    val actionbar = getActionBar
+    actionbar.setTitle(nickname)
+
+    // TODO: Include rank one day?
+    //actionbar.setSubtitle(pkg.summary)
+
+    val bytes = s"${nickname}@fedoraproject.org".getBytes("utf8")
+    Cache.getGravatar(
+      this,
+      Hashing.md5.hashBytes(bytes).toString).onComplete { result =>
+        result match {
+          case Success(gravatar) =>
+            runOnUiThread(actionbar.setIcon(new BitmapDrawable(getResources, gravatar)))
+          case _ =>
+        }
+    }
 
     Badges.query(s"/user/${nickname}/json") onComplete {
       case Success(res) => {
