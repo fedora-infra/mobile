2a017b6ea7b19d359a5bf72d08b01206354fd109
Ricky Elrod <ricky@elrod.me>
2013-11-22 23:04:19 -0500

    Better icons for badges and preferences

--- src/main/scala/activity/NavDrawerActivity.scala
+++ src/main/scala/activity/NavDrawerActivity.scala
@@ -17,9 +17,9 @@ trait NavDrawerActivity extends FragmentActivity with TypedActivity {
   lazy val navMap = Map(
     getString(R.string.infrastructure_status) -> (classOf[StatusActivity], R.drawable.ic_status),
     getString(R.string.package_search) -> (classOf[PackageSearchActivity], R.drawable.ic_search),
-    getString(R.string.badges_leaderboard) -> (classOf[BadgesLeaderboardActivity], R.drawable.ic_fedocal),
-    getString(R.string.fedmsg_gcm_demo) -> (classOf[GCMDemoActivity], R.drawable.ic_fedmsg),
-    getString(R.string.preferences) -> (classOf[PreferencesActivity], R.drawable.ic_fedocal)
+    getString(R.string.badges_leaderboard) -> (classOf[BadgesLeaderboardActivity], R.drawable.ic_badges),
+//    getString(R.string.fedmsg_gcm_demo) -> (classOf[GCMDemoActivity], R.drawable.ic_fedmsg),
+    getString(R.string.preferences) -> (classOf[PreferencesActivity], R.drawable.ic_preferences)
   )
 
   override def onCreate(bundle: Bundle) {
