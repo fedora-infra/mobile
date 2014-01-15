aae48d18cc114a7d13f59e831ba68037f4b08615
Ricky Elrod <ricky@elrod.me>
2014-01-14 20:29:57 -0500

    Make the widget prettier

--- src/main/scala/widget/FedmsgCountWidget.scala
+++ src/main/scala/widget/FedmsgCountWidget.scala
@@ -10,7 +10,7 @@ import android.widget.RemoteViews
 import scalaz._, Scalaz._
 import scalaz.concurrent.Promise
 
-import java.text.SimpleDateFormat
+import java.text.{ NumberFormat, SimpleDateFormat }
 import java.util.Date
 
 class FedmsgCountWidgetProvider extends AppWidgetProvider {
@@ -21,9 +21,11 @@ class FedmsgCountWidgetProvider extends AppWidgetProvider {
         case -\/(err) => Log.e("FedmsgCountWidgetProvider", err)
         case \/-(mc) => {
           val views: RemoteViews = new RemoteViews(context.getPackageName, R.layout.widget_fedmsg_count)
-          val time: String = new SimpleDateFormat("EEEEE, MMM d, yyyy h:mm a").format(new Date)
-          views.setTextViewText(R.id.count, mc.messagecount.toString)
-          views.setTextViewText(R.id.count_time, time.toString)
+          val time: String = new SimpleDateFormat("h:mm a").format(new Date)
+          val label: String = context.getString(R.string.fedmsg_count_time).format(time)
+          val fedmsgCount: String = NumberFormat.getInstance.format(mc.messagecount)
+          views.setTextViewText(R.id.count, fedmsgCount)
+          views.setTextViewText(R.id.fedmsg_count_label, label)
           appWidgetManager.updateAppWidget(n, views)
         }
       }
