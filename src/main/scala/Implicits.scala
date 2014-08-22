9f412f9a0ee6d320c21161653854e26e4a4a5913
Ricky Elrod <ricky@elrod.me>
2014-08-22 16:07:46 -0400

    Nuke .tap, this was never a good idea
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/main/scala/Implicits.scala
+++ src/main/scala/Implicits.scala
@@ -9,29 +9,4 @@ package object Implicits {
       ()
     }
   }
-
-  /** A rough implementation of Ruby's "tap" method.
-    *
-    * This method lets you "tap" objects which don't return themselves when you
-    * call their setters.
-    *
-    * For example, Android's [[android.widget.EditText]] doesn't return the
-    * EditText when you call setHint(). setHint instead returns nothing (void).
-    *
-    * This "tap" method lets you work around this by doing something like:
-    *
-    * {{{
-    * val et = new EditText(context).tap { obj =>
-    * obj.setHint(R.string.hint)
-    * obj.setSingleLine(true)
-    * }
-    * // et is now an EditText, but those setters have been invoked on it.
-    * }}}
-    */
-  implicit def anyToTap[A](that: A) = new {
-    def tap(f: (A) => Unit): A = {
-      f(that)
-      that
-    }
-  }
 }
