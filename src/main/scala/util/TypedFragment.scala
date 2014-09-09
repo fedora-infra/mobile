78d210ecd80ec0c33b11d2834253951c51767a8c
Ricky Elrod <ricky@elrod.me>
2014-09-09 18:25:07 -0400

    holy monads!
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- src/main/scala/util/TypedFragment.scala
+++ src/main/scala/util/TypedFragment.scala
@@ -1,11 +1,19 @@
 package org.fedoraproject.mobile
 
 import android.app.Fragment
+import android.content.Context
 import android.view.View
+
+import scalaz.concurrent.Task
 
 trait TypedFragment extends Fragment with TypedActivityHolder {
   def findViewById(x: Int): View = getView.findViewById(x)
   def activity = getActivity
   def runOnUiThread(x: Runnable): Unit = activity.runOnUiThread(x)
   def findViewOpt[T](tr: TypedResource[T]) = Option(findView(tr))
+
+  // Rather than passing context around manually everywhere, just make it
+  // implicit here.
+  implicit val ctxTask: Task[Context] =
+    Task { getActivity.getApplicationContext }
 }
